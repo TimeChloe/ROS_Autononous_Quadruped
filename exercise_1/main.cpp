0c8b79e2ec573fc30a22ed2ff5f45a2e37001121
Yue <tum22.zhang@tum.de>
2024-05-06 14:51:57 -0700

    exercise_1 calculator

---
+++ exercise_1/main.cpp
@@ -0,0 +1,15 @@
+
+#include "calculator.h"
+#include <iostream>
+
+
+int main() {
+
+  MyMath math_;
+
+  // Implement Input
+
+  std::cout << "Sum is =  " << math_.Sum_(1.0, 1.0) << std::endl;
+
+  return 0;
+}
